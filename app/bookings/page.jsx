036f42615a4585897afc890752d1cc2e04b31a44
TariqKichawele <tariqkichawele66@gmail.com>
2024-10-14 14:33:55 -0400

    implemented getMyBookings and CancelBooking action

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import getMyBookings from '../actions/getMyBookings'
+import Heading from '@/components/Heading'
+import BookedRoomCard from '@/components/BookedRoomCard'
 
-const Bookings = () => {
+const Bookings = async () => {
+  const bookings = await getMyBookings()
   return (
-    <div>Bookings</div>
+    <>
+    <Heading title='My Bookings' />
+    {bookings.length === 0 ? (
+      <p className='text-gray-600 mt-4'>You have no bookings</p>
+      ) : (
+        bookings.map((booking) => (
+          <BookedRoomCard key={booking.$id} booking={booking} />
+        ))
+    )}
+  </>
   )
 }
 
